d15d3ecac36b6b5093741a62912047162aead96c
Mohammed Hammad <mohamedhammad923@gmail.com>
2024-12-05 19:48:32 +0200

    tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+      "./public/**/*.php",
+      "./app/template/**/*.php",
+      "./app/views/**/*.php",    
+  ],
   theme: {
     extend: {},
   },
