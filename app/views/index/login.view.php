26e0922bab29a460cb1c99a4b6e92a5bb5fc9589
Mohammed Hammad <mohamedhammad923@gmail.com>
2024-12-07 13:54:49 +0200

    add login button

--- app/views/index/login.view.php
+++ app/views/index/login.view.php
@@ -32,24 +32,27 @@
             <!-- Social Media Login -->
             <div class="flex flex-col sm:flex-row justify-center gap-4 mb-6">
                 <!-- Google Login -->
-                <a href="/auth/google"
-                    class="w-full sm:w-auto inline-flex items-center justify-center px-6 py-3 text-sm font-medium text-black bg-white hover:bg-white rounded-lg shadow-lg focus:outline-none focus:ring-2 focus:ring-white transition-transform transform hover:scale-105">
-                    <svg class="w-5 h-5 mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
-                        <path fill="#EA4335"
-                            d="M24 9.5c3.59 0 6.84 1.32 9.35 3.49l7.02-7.03C36.58 2.05 30.69 0 24 0 14.49 0 6.33 5.55 2.77 13.5l8.49 6.59C13.31 14.3 18.32 9.5 24 9.5z" />
-                        <path fill="#34A853"
-                            d="M46.1 24.5c0-1.77-.18-3.49-.51-5.17H24v9.77h12.45c-1.08 3.68-4.02 6.67-7.65 7.81v6.5h12.38c7.23-6.63 11.37-16.39 11.37-27.41z" />
-                        <path fill="#4A90E2"
-                            d="M24 48c6.48 0 11.91-2.14 15.87-5.78l-12.38-6.5c-1.56 1.01-3.57 1.6-5.5 1.6-5.68 0-10.69-4.8-11.74-11.21l-8.51 6.58c3.57 7.95 11.73 13.5 21.26 13.5z" />
-                        <path fill="#FBBC05"
-                            d="M2.77 13.5c-1.45 3.01-2.27 6.39-2.27 9.99s.82 6.98 2.27 9.99l8.49-6.58c-.57-1.72-.89-3.58-.89-5.41s.32-3.69.89-5.41l-8.49-6.58z" />
-                    </svg>
-                    Continue with Google
-                </a>
+                <div id="g_id_onload"
+                    data-client_id="281698038178-al3jc4eqjf8nie8buk55djra2bqk7841.apps.googleusercontent.com"
+                    data-context="signin"
+                    data-ux_mode="popup"
+                    data-login_uri="loginCallback"
+                    data-itp_support="true">
+                </div>
+
+                <div class="g_id_signin"
+                    data-type="standard"
+                    data-shape="pill"
+                    data-theme="outline"
+                    data-text="continue_with"
+                    data-size="large"
+                    data-locale="en-US"
+                    data-logo_alignment="left">
+                </div>
 
                 <!-- Facebook Login -->
                 <a href="/auth/facebook"
-                    class="w-full sm:w-auto inline-flex items-center justify-center px-6 py-3 text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 rounded-lg shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-400 transition-transform transform hover:scale-105">
+                    class="w-full sm:w-auto flex items-center justify-center px-4 rounded-full text-sm font-medium text-white bg-blue-600">
                     <svg class="w-5 h-5 mr-3" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
                         <path
                             d="M22.676 0H1.324C.594 0 0 .594 0 1.324v21.352C0 23.406.594 24 1.324 24H12v-9.293H9.293V11.5H12V8.793c0-2.689 1.548-4.207 3.915-4.207 1.136 0 2.332.205 2.332.205v2.573h-1.311c-1.294 0-1.693.804-1.693 1.627V11.5h3.012l-.481 3.207h-2.531V24h4.953c.73 0 1.324-.594 1.324-1.324V1.324C24 .594 23.406 0 22.676 0z" />
